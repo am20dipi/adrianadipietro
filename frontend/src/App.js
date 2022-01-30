dac312fccae3deee7b1b84b8ff51471e144d82df
Adriana <am20dipi@alum.siena.edu>
2022-01-30 12:52:55 -0500

    Installing np bootstrap; building out navbar using bootstrap; creating Home.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-import Navbar from './Navbar';
-
+import Navbar from './Navbar.js'
+import Home from './Home.js'
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <h1>ADRIANA DIPIETRO</h1>
+      
+      <BrowserRouter>
       <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,31 +1,32 @@
-
+import { Link } from 'react-router-dom'
+import React from 'react'
 
 function Navbar(){
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <Link class="navbar-brand" href="#">Navbar</Link>>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="#">Navbar</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
         </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <Link class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+                <li className="nav-item active">
+                    <Link className="nav-link" to="#">Home</Link> {/* <span className="sr-only">(current)</span> */}
                 </li>
-                <li class="nav-item">
-                    <Link class="nav-link" href="#">Features</a>
+                <li className="nav-item">
+                    <Link className="nav-link" to="#">About</Link>
                 </li>
-                <li class="nav-item">
-                    <Link class="nav-link" href="#">Pricing</a>
+                <li className="nav-item">
+                    <Link className="nav-link" to="#">Contact</Link>
                 </li>
-                <li class="nav-item dropdown">
-                    <Link class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <li className="nav-item dropdown">
+                    <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Dropdown link
                     </Link>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <Link class="dropdown-item" href="#">Action</Link>>
-                    <Link class="dropdown-item" href="#">Another action</Link>>
-                    <Link class="dropdown-item" href="#">Something else here</Link>>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <Link className="dropdown-item" to="#">Action</Link>
+                        <Link className="dropdown-item" to="#">Another action</Link>
+                        <Link className="dropdown-item" to="#">Something else here</Link>
                     </div>
                 </li>
                 </ul>
